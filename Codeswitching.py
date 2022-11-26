cb44a4a097de94f213fa6c1781c3561f12d174bd
rahim_mazouz <42543749+aspirant2018@users.noreply.github.com>
2022-11-26 09:00:38 +0100

    log10

--- Codeswitching.py
+++ Codeswitching.py
@@ -1,6 +1,7 @@
 from random import choice
 from typing import List, Tuple
 from random import shuffle
+from math import log10
 
 
 
@@ -29,4 +30,40 @@ for tweet in corpus:
 shuffle(filtered_corpus)
 train_set = filtered_corpus[:int(.8 * len(filtered_corpus))]
 test_set = filtered_corpus[int(.8 * len(filtered_corpus)):]
+
+
+
+
+en_corpus=[]
+all_en_char = []
+for tweet in train_set:
+    for mot,tag in tweet:
+        if tag =="en":
+            en_corpus.append(mot)
+for mot in en_corpus:
+    for c in mot:
+        all_en_char.append(c)
+all_en_char =Counter(all_en_char)
+print(all_en_char)
+
+
+hi_corpus=[]
+all_hi_char = []
+for tweet in train_set:
+    for mot,tag in tweet:
+        if tag =="hi":
+            hi_corpus.append(mot)
+for mot in hi_corpus:
+    for c in mot:
+        all_hi_char.append(c)
+all_hi_char =Counter(all_hi_char)
+print(all_hi_char)
+
+
+
+all_en_char = {k: log10(v / sum(all_en_char.values())) for k, v in all_en_char.items()}
+all_hi_char = {k: log10(v / sum(all_hi_char.values()))for k, v in all_hi_char.items()}
+all_univ_char = {k:log10(v / sum(all_univ_char.values())) for k, v in all_univ_char.items()}
+
+
 
