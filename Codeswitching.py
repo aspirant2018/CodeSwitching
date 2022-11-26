1854ee16ebc4146f5850a9f51ef5de76b4268ad1
rahim_mazouz <42543749+aspirant2018@users.noreply.github.com>
2022-11-26 11:36:42 +0100

    code switching

--- Codeswitching.py
+++ Codeswitching.py
@@ -2,7 +2,7 @@ from random import choice
 from typing import List, Tuple
 from random import shuffle
 from math import log10
-
+from collections import Counter
 
 
 def read_data(content : str) -> List[List[Tuple[str, str]]]:
@@ -16,6 +16,14 @@ def read_data(content : str) -> List[List[Tuple[str, str]]]:
     return corpus
 
 corpus = read_data(open("TWT_HI_EN_FN.txt", "r", encoding="utf-8").read())
+all_tags = []
+
+for tweet in corpus:
+    for word, tag in tweet:
+        all_tags.append(tag)
+
+
+Counter(all_tags)
 
 filtered_corpus = []
 for tweet in corpus:
@@ -65,5 +73,39 @@ all_en_char = {k: log10(v / sum(all_en_char.values())) for k, v in all_en_char.i
 all_hi_char = {k: log10(v / sum(all_hi_char.values()))for k, v in all_hi_char.items()}
 all_univ_char = {k:log10(v / sum(all_univ_char.values())) for k, v in all_univ_char.items()}
 
+def probabilite_en(mot):
+    '''
+    une fonction qui retourne la probabilté d'un mot dans la langue anglaise P(w/Lan)
+    '''
+    Pe=0
+    for l in mot:
+        for c in all_en_char.keys():
+            if l == c:
+                Pe=Pe+all_en_char.get(c,-10)
+    return Pe
 
+'''
+    Écrivez une fonction qui prend en entrée un mot et qui renvoie
+    `en` ou `hi` en fonction de la règle de décision exposée ci-dessus
+'''
+def classifieur(mot):
+    probabilite_hi(mot)
+    probabilite_en(mot)
+    if probabilite_hi(mot) >= probabilite_en(mot):
+        return "hi"
+    else:
+        return "en"
+
+predict_correct=0
+predict_fausse=0
+langue=''
+for tweet in test_set:
+    for word,tag in tweet:
+        if tag!='univ':
+            langue= classifieur(word)
+            if langue== tag:
+                predict_correct+=1
+            else:
+                predict_fausse+=1
+print("nombre de predictions corrects est ",predict_correct/(predict_fausse+predict_correct))
 
